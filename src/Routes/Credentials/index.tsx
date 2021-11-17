7a593348ff52e3544212b505aedc66008f74e2a0
Amanmalik444 <Amanmaliknhr@gmail.com>
2021-11-17 22:35:58 +0530

    mayank bhai op

--- src/Routes/Credentials/index.tsx
+++ src/Routes/Credentials/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Login from "./Components/LoginPage";
 import Register from "./Components/RegistrationPage";
-import Canvas  from "../../Components/SineWave/components/canvas";
 
 interface IState {
   pageToShow: string;
@@ -12,15 +11,12 @@ const Credentials = () => {
     React.useState<IState["pageToShow"]>("login");
 
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-gray-900 ">
+    <div className="h-screen w-full flex justify-center items-center bg-gray-700">
       {pageToShow === "login" ? (
         <Login setPageToShow={setPageToShow} />
       ) : (
         <Register setPageToShow={setPageToShow} />
       )}
-      <div className="absolute">
-        <Canvas />
-      </div>
     </div>
   );
 };
