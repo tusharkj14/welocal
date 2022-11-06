7c06d6f4b2bcf404d24abebb8812c6cb67a4b56f
Tushar Kanti Jaiswal <karanwalia2001@gmail.com>
2022-11-06 08:35:17 +0530

    Update index.tsx

--- src/Store/Actions/UserAction/index.tsx
+++ src/Store/Actions/UserAction/index.tsx
@@ -3,6 +3,7 @@ import * as actionTypes from "./types";
 
 type dispatchType = Dispatch<actionTypes.UserDispatchTypes>;
 
+
 export const setUser = (user: any) => {
   return {
     type: "SET_USER",
